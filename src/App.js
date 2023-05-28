cc0d1fa084617fd73e129eb72de20185efb6db64
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-28 14:26:50 +0300

    Классовый компонент ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 function App() {
 	return (
 		<div className="App">
-			<Counter />
-			<Counter />
-			<Counter />
-			<Counter />
+			<ClassCounter/>
+			<ClassCounter/>
+			<ClassCounter/>
 		</div>
 	);
 }
