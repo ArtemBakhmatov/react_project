3343d7756a4c003002ef2e3ec4673ec2af6297ab
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-28 18:05:37 +0300

    Props. Аргументы компонента App

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import React from "react";
 
 import './styles/App.css';
+import PostItem from "./components/PostItem";
 
 function App() {
 	return (
 		<div className="App">
-			<div className="post">
-				<div className="post__content">
-					<strong>1. JavaScript</strong>
-					<div>
-						JavaScript - Язык програморивания
-					</div>
-				</div>
-				<div className="post__btns">
-					<button>Удалить</button>
-				</div>
-			</div>
-
-			<div className="post">
-				<div className="post__content">
-					<strong>1. JavaScript</strong>
-					<div>
-						JavaScript - Язык програморивания
-					</div>
-				</div>
-				<div className="post__btns">
-					<button>Удалить</button>
-				</div>
-			</div>
-
-			<div className="post">
-				<div className="post__content">
-					<strong>1. JavaScript</strong>
-					<div>
-						JavaScript - Язык програморивания
-					</div>
-				</div>
-				<div className="post__btns">
-					<button>Удалить</button>
-				</div>
-			</div>
+			<PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}} />
+			<PostItem post={{id: 2, title: 'HTML', body: 'Description'}} />
+			<PostItem post={{id: 3, title: 'CSS', body: 'Description'}} />
+			<PostItem post={{id: 4, title: 'React', body: 'Description'}} />
+			<PostItem post={{id: 5, title: 'React Native', body: 'Description'}} />
 		</div>
 	);
 }
