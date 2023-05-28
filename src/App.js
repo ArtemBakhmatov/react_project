2630ba33fee09dcb476de16dd9b39f3f7038fe71
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-28 17:49:19 +0300

    Стили CSS. Классы

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+
+import './styles/App.css';
+
 function App() {
 	return (
 		<div className="App">
-			<ClassCounter/>
-			<ClassCounter/>
-			<ClassCounter/>
+			<div className="post">
+				<div className="post__content">
+					<strong>1. JavaScript</strong>
+					<div>
+						JavaScript - Язык програморивания
+					</div>
+				</div>
+				<div className="post__btns">
+					<button>Удалить</button>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post__content">
+					<strong>1. JavaScript</strong>
+					<div>
+						JavaScript - Язык програморивания
+					</div>
+				</div>
+				<div className="post__btns">
+					<button>Удалить</button>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post__content">
+					<strong>1. JavaScript</strong>
+					<div>
+						JavaScript - Язык програморивания
+					</div>
+				</div>
+				<div className="post__btns">
+					<button>Удалить</button>
+				</div>
+			</div>
 		</div>
 	);
 }
