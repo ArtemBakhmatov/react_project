ff1558b2177ca9deb7fcea1dea7e9facda3ff421
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-28 19:38:08 +0300

    Создание UI своей библиотеки и управл-й компонент 2-го инпута в форме

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import './styles/App.css';
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -10,19 +12,32 @@ function App() {
 		{id: 3, title: 'CSS', body: 'Description'}
 	]);
 
-	const [posts2, setPosts2] = useState([
-		{id: 1, title: 'Python', body: 'Description'},
-		{id: 2, title: 'C++', body: 'Description'},
-		{id: 3, title: 'Java', body: 'Description'}
-	]);
+	const [title, setTitle] = useState('');
+
+	const addNewPost = (e) => {
+		e.preventDefault();
+		console.log(title)
+	}
+
 
 	return (
 		<div className="App">
-			<PostList posts={posts} title='Посты про вебразработку' />
-			<PostList posts={posts2} title='Посты про програмирование' />
-			
+			<form>
+				{/* Управляемый компонент */}
+				<MyInput 
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+					type="text" 
+					placeholder="Название поста" 
+				/>
+				<MyInput type="text" placeholder="Описание поста" />
+				<MyButton onClick={addNewPost}>Создать пост</MyButton>
+			</form>
+			<PostList posts={posts} title='Посты про вебразработку' />	
 		</div>
 	);
+
+	// type='submit' нужен когда мы отправляем на сервер, сейчас он нам не нужен
 }
 
 export default App;
