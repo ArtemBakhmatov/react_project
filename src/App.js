1ab9899e034f065a8fd0adb1a9d4f8f22a726e06
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-28 21:11:54 +0300

    Для всех инпутов сделали одно состояние(тут посложнее)

--- src/App.js
+++ src/App.js
@@ -12,21 +12,15 @@ function App() {
 		{id: 3, title: 'CSS', body: 'Description'}
 	]);
 
-	const [title, setTitle] = useState('');
-	const [body, setBody] = useState('');
+	const [post, setPost] = useState({title: '', body: ''});
 
 	const addNewPost = (e) => {
 		e.preventDefault();
-		const newPost = {
-			id: Date.now(),
-			title, 
-			body
-		}
-		setPosts([...posts, newPost]); 
+	
+		setPosts([...posts, {...post, id: Date.now()}]); 
 		// 1 аргумент разворачивает все посты
 		// 2 аргумент вставляет новые данные данные в 1 аргумент
-		setTitle('');
-		setBody('');
+		setPost({title: '', body: ''});
 
 	}
 
@@ -36,14 +30,14 @@ function App() {
 			<form>
 				{/* Управляемый компонент */}
 				<MyInput 
-					value={title}
-					onChange={e => setTitle(e.target.value)}
+					value={post.title}
+					onChange={e => setPost({...post, title: e.target.value})}
 					type="text" 
 					placeholder="Название поста" 
 				/>
 				<MyInput 
-					value={body}
-					onChange={e => setBody(e.target.value)}
+					value={post.body}
+					onChange={e => setPost({...post, body: e.target.value})}
 					placeholder="Описание поста" />
 				<MyButton onClick={addNewPost}>Создать пост</MyButton>
 			</form>
