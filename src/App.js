772b0b465173a4733b1df41491d72454b8385cb1
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-28 18:41:04 +0300

    Списки. Вызываем список через массив объектов

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './styles/App.css';
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
+	const [posts, setPosts] = useState([
+		{id: 1, title: 'JavaScript', body: 'Description'},
+		{id: 2, title: 'HTML', body: 'Description'},
+		{id: 3, title: 'CSS', body: 'Description'}
+	]);
+
+	const [posts2, setPosts2] = useState([
+		{id: 1, title: 'Python', body: 'Description'},
+		{id: 2, title: 'C++', body: 'Description'},
+		{id: 3, title: 'Java', body: 'Description'}
+	]);
+
 	return (
 		<div className="App">
-			<PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}} />
-			<PostItem post={{id: 2, title: 'HTML', body: 'Description'}} />
-			<PostItem post={{id: 3, title: 'CSS', body: 'Description'}} />
-			<PostItem post={{id: 4, title: 'React', body: 'Description'}} />
-			<PostItem post={{id: 5, title: 'React Native', body: 'Description'}} />
+			<PostList posts={posts} title='Посты про вебразработку' />
+			<PostList posts={posts2} title='Посты про програмирование' />
+			
 		</div>
 	);
 }
