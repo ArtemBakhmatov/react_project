cd7a08b377d4b19ed13d47667929deef891e4e23
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-30 18:58:04 +0300

    Работа с сервером. Axios

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/myModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hook/usePosts";
+import axios from "axios";
 
 function App() {
-	const [posts, setPosts] = useState([
-		{id: 1, title: 'JavaScript', body: 'Descriptionqwedq'},
-		{id: 2, title: 'HTML', body: 'Descriptioqwfefwn'},
-		{id: 3, title: 'CSS', body: 'Descriptionwqefgseewe'}
-	]);
+	const [posts, setPosts] = useState([]);
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
 		setModal(false);
+	}
+
+	async function fetchPosts() {
+		const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+		setPosts(response.data);
 	}
 
 	// Получаем post из дочернего элемента	
@@ -31,6 +33,7 @@ function App() {
 	
 	return (
 		<div className="App">
+			<button onClick={fetchPosts}>GET POSTS</button>
 			<MyButton 
 				style={{marginTop: '30px'}}
 				onClick={() => setModal(true)}>
