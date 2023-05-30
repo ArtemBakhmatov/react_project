d81db097753bb76ae9ca7874214bbc66d05193b0
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-30 21:51:08 +0300

    Создание спиннера

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ import MyModal from "./components/UI/myModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hook/usePosts";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
 
 function App() {
 	const [posts, setPosts] = useState([]);
+	const [filter, setFilter] = useState({sort: '', query: ''});
+	const [modal, setModal] = useState(false);
+	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+	const [isPostLoading, setIsPostLoading] = useState(false);
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -18,18 +23,19 @@ function App() {
 	}
 
 	async function fetchPosts() {
-		const posts = await PostService.getAll();
-		setPosts(posts);
+		setIsPostLoading(true);
+		setTimeout(async () => {
+			const posts = await PostService.getAll();
+			setPosts(posts);
+			setIsPostLoading(false);
+		}, 1000);
+		
 	}
 
 	// Получаем post из дочернего элемента	
 	const removePost = (post) => {
 		setPosts(posts.filter(p => p.id !== post.id));
 	}
-
-	const [filter, setFilter] = useState({sort: '', query: ''});
-	const [modal, setModal] = useState(false);
-	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
 	useEffect(() => {
 		fetchPosts();
@@ -51,7 +57,15 @@ function App() {
 				filter={filter} 
 				setFilter={setFilter} 
 			/>
-			<PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про вебразработку' />	
+			{isPostLoading
+				? 
+					<div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+						<Loader />
+					</div>
+				:
+					<PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про вебразработку' />	
+			}
+			
 		</div>
 	);
 }
