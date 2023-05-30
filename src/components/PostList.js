3b7ab8eee00eb34f25be8d4bf8d9559321908375
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-30 17:53:55 +0300

    React Transition Group

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PostItem from './PostItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const PostList = ({remove, posts, title}) => {
     if (!posts.length) {
@@ -17,9 +18,18 @@ const PostList = ({remove, posts, title}) => {
             <h1 style={{textAlign: 'center'}}>
 				{title}
 			</h1>
-			{posts.map((post, i) => 
-				<PostItem remove={remove} number={i + 1} post={post} key={post.id} />
-			)}
+            <TransitionGroup>
+                {posts.map((post, i) => 
+                <CSSTransition
+                    key={post.id}
+                    timeout={500}
+                    classNames='post'
+                >
+                    <PostItem remove={remove} number={i + 1} post={post} key={post.id} />
+                </CSSTransition>
+                    
+                )}
+            </TransitionGroup>
         </div>
     );
 };
