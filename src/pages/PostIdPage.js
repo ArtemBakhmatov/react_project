f7d1b8c69f123a9260b16e94395fdfe047795a08
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-05-31 22:17:10 +0300

    Загрузка комментриев к посту, приватные и публичные маршруты

--- src/pages/PostIdPage.js
+++ src/pages/PostIdPage.js
@@ -7,13 +7,22 @@ import Loader from '../components/UI/loader/Loader';
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+
     const [fetchPostById, isLoading, error] = useFething(async (id) => {
-        const response = await PostService.getById(id);
+        const response = await PostService.getById(id)
         setPost(response.data);
     })
+    const [fetchComments, isComLoading, comError] = useFething(async (id) => {
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data);
+    })
+
     useEffect(() => {
         fetchPostById(params.id);
-    }, [])
+        fetchComments(params.id);
+    }, []);
+
     return (
         <div>
             <h1>Вы открыли страницу поста c ID = {params.id}</h1>
@@ -23,6 +32,22 @@ const PostIdPage = () => {
                 :
                 <div>{post.id}, {post.title}</div>
             }  
+            <h1>
+                Комментарии
+            </h1>
+            {isComLoading
+                ? 
+                    <Loader/>
+                : 
+                    <div>
+                        {comments.map(comm =>
+                            <div key={comm.id} style={{marginTop: 15}}>
+                                <h5>{comm.email}</h5>
+                                <div>{comm.body}</div>
+                            </div>
+                        )}
+                    </div>
+            }
         </div>
     );
 };
